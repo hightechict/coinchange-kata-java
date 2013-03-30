c3f8c1ebd81c742358ca85f8fb7bd56d9f473bee
Jaime Visser <jaimevisser@gmail.com>
2013-03-30 16:55:08 +0100

    Single coin

--- src/main/java/nl/hightech/dojo/coinchangekata/Coinchanger.java
+++ src/main/java/nl/hightech/dojo/coinchangekata/Coinchanger.java
@@ -1,15 +1,19 @@
 package nl.hightech.dojo.coinchangekata;
 
-import java.util.HashSet;
+import java.util.Set;
 
+import com.google.common.collect.HashMultiset;
 import com.google.common.collect.Multiset;
 
 public class Coinchanger implements ICoinchanger {
 
-	public Coinchanger(HashSet<Integer> hashSet) {}
+	public Coinchanger(Set<Integer> coinSet) {}
 
 	public Multiset<Integer> getChangeForAmount(int amountInCents) {
-		return null;
+		Multiset<Integer> change = HashMultiset.create();
+		change.add(1);
+
+		return change;
 	}
 
 }
--- src/test/java/nl/hightech/dojo/coinchangekata/CoinchangerTest.java
+++ src/test/java/nl/hightech/dojo/coinchangekata/CoinchangerTest.java
@@ -1,13 +1,28 @@
 package nl.hightech.dojo.coinchangekata;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.HashSet;
 
 import org.junit.Test;
 
+import com.google.common.collect.Multiset;
+import com.google.common.collect.Sets;
+
 public class CoinchangerTest {
+
+	Coinchanger coinchanger;
+	Multiset<Integer> change;
 
 	@Test
 	public void testConstructor() {
 		new Coinchanger(new HashSet<Integer>());
+	}
+
+	@Test
+	public void testSingleCoin() {
+		coinchanger = new Coinchanger(Sets.newHashSet(1, 5, 10));
+		change = coinchanger.getChangeForAmount(1);
+		assertEquals(1, change.size());
 	}
 }
