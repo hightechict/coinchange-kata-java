c313f48d4de7ab19a85674bc525a847875c75a03
Jaime Visser <jaimevisser@gmail.com>
2013-04-01 15:42:55 +0200

    Eight cents

--- src/main/java/nl/hightech/dojo/coinchangekata/Coinchanger.java
+++ src/main/java/nl/hightech/dojo/coinchangekata/Coinchanger.java
@@ -1,19 +1,47 @@
 package nl.hightech.dojo.coinchangekata;
 
+import java.util.Collections;
 import java.util.Set;
+import java.util.TreeSet;
 
 import com.google.common.collect.HashMultiset;
 import com.google.common.collect.Multiset;
+import com.google.common.collect.Multiset.Entry;
 
 public class Coinchanger implements ICoinchanger {
 
-	public Coinchanger(Set<Integer> coinSet) {}
+	TreeSet<Integer> coinSet;
+
+	public Coinchanger(Set<Integer> coinSet) {
+		this.coinSet = new TreeSet<Integer>(Collections.reverseOrder());
+		this.coinSet.addAll(coinSet);
+	}
 
 	public Multiset<Integer> getChangeForAmount(int amountInCents) {
+
 		Multiset<Integer> change = HashMultiset.create();
-		change.add(1);
+
+		while (valueOf(change) < amountInCents) {
+			change.add(largestFittingCoin(amountInCents - valueOf(change)));
+		}
 
 		return change;
+	}
+
+	private int largestFittingCoin(int amount) {
+		for (int c : coinSet) {
+			if (c <= amount) return c;
+		}
+
+		throw new IllegalArgumentException();
+	}
+
+	private static int valueOf(Multiset<Integer> coinSet) {
+		int t = 0;
+		for (Entry<Integer> coin : coinSet.entrySet()) {
+			t += coin.getCount() * coin.getElement();
+		}
+		return t;
 	}
 
 }
--- src/test/java/nl/hightech/dojo/coinchangekata/CoinchangerTest.java
+++ src/test/java/nl/hightech/dojo/coinchangekata/CoinchangerTest.java
@@ -3,6 +3,7 @@ package nl.hightech.dojo.coinchangekata;
 import static org.junit.Assert.assertEquals;
 
 import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Test;
 
@@ -14,6 +15,8 @@ public class CoinchangerTest {
 	Coinchanger coinchanger;
 	Multiset<Integer> change;
 
+	Set<Integer> euroCoinSet = Sets.newHashSet(1, 2, 5, 10, 20, 50, 100, 200);
+
 	@Test
 	public void testConstructor() {
 		new Coinchanger(new HashSet<Integer>());
@@ -21,8 +24,23 @@ public class CoinchangerTest {
 
 	@Test
 	public void testSingleCoin() {
-		coinchanger = new Coinchanger(Sets.newHashSet(1, 5, 10));
+		coinchanger = new Coinchanger(euroCoinSet);
+
 		change = coinchanger.getChangeForAmount(1);
+
 		assertEquals(1, change.size());
+		assertEquals(1, change.count(1));
+	}
+
+	@Test
+	public void testEightCents() {
+		coinchanger = new Coinchanger(euroCoinSet);
+
+		change = coinchanger.getChangeForAmount(8);
+
+		assertEquals(3, change.size());
+		assertEquals(1, change.count(1));
+		assertEquals(1, change.count(2));
+		assertEquals(1, change.count(5));
 	}
 }
